1b6ca22af437f84333ad373445b23cc75015c3db
Rohit9502 <123442986+Rohit9502@users.noreply.github.com>
2023-01-24 12:20:41 +0530

    by Mayur

--- ABC1/src/com/Test.java
+++ ABC1/src/com/Test.java
@@ -2,9 +2,17 @@ package com;
 
 public class Test 
 {
+	public void m1()
+	{
+		System.out.println("Welcome");
+	
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("hello");
+		Test t= new Test();
+		t.m1();
 		
 	}
 
